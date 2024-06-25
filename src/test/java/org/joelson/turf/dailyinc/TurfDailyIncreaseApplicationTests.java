ef707bfba9b0314d7a93070920dbe21b92d77186
Mattias Joëlson <mattias@joelson.org>
2024-06-25 08:48:16 +0200

    Boiler plate code from start.spring.io.

---
+++ src/test/java/org/joelson/turf/dailyinc/TurfDailyIncreaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.joelson.turf.dailyinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurfDailyIncreaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
