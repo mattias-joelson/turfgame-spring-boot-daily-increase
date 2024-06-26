a21f92e3e02ad1383a60da2f78d6931576e95f02
Mattias Joëlson <mattias@joelson.org>
2024-06-26 11:20:15 +0200

    Remove Customer example.

--- src/main/java/org/joelson/turf/dailyinc/TurfDailyIncreaseApplication.java
+++ src/main/java/org/joelson/turf/dailyinc/TurfDailyIncreaseApplication.java
@@ -1,7 +1,5 @@
 package org.joelson.turf.dailyinc;
 
-import org.joelson.turf.dailyinc.model.Customer;
-import org.joelson.turf.dailyinc.model.CustomerRepository;
 import org.joelson.turf.dailyinc.service.FeedImporterService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -67,36 +65,5 @@ public class TurfDailyIncreaseApplication {
         } else {
             logger.info(noElements);
         }
-    }
-
-    //@Bean
-    public CommandLineRunner demo(CustomerRepository repository) {
-        return (args) -> {
-            // save a few customers
-            repository.save(new Customer("Jack", "Bauer"));
-            repository.save(new Customer("Chloe", "O'Brian"));
-            repository.save(new Customer("Kim", "Bauer"));
-            repository.save(new Customer("David", "Palmer"));
-            repository.save(new Customer("Michelle", "Dessler"));
-
-            // fetch all customers
-            logger.info("Customers found with findAll():");
-            logger.info("-------------------------------");
-            repository.findAll().forEach(customer -> logger.info(customer.toString()));
-            logger.info("");
-
-            // fetch an individual customer by ID
-            Customer customer = repository.findById(1L);
-            logger.info("Customer found with findById(1L):");
-            logger.info("--------------------------------");
-            logger.info(customer.toString());
-            logger.info("");
-
-            // fetch customers by last name
-            logger.info("Customer found with findByLastName('Bauer'):");
-            logger.info("--------------------------------------------");
-            repository.findByLastName("Bauer").forEach(bauer -> logger.info(bauer.toString()));
-            logger.info("");
-        };
     }
 }
