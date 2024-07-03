2288b857804d1efdb98ed4a81cae9fbfa6bbb652
Mattias Joëlson <mattias@joelson.org>
2024-07-03 11:49:43 +0200

    Messy testing...

--- src/test/java/org/joelson/turf/dailyinc/api/VisitAPIServiceTest.java
+++ src/test/java/org/joelson/turf/dailyinc/api/VisitAPIServiceTest.java
@@ -1,5 +1,6 @@
 package org.joelson.turf.dailyinc.api;
 
+import org.hibernate.query.spi.Limit;
 import org.joelson.turf.dailyinc.model.User;
 import org.joelson.turf.dailyinc.model.Visit;
 import org.joelson.turf.dailyinc.model.VisitRepository;
@@ -7,15 +8,24 @@ import org.joelson.turf.dailyinc.model.VisitType;
 import org.joelson.turf.dailyinc.model.Zone;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatcher;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.internal.matchers.Equals;
+import org.mockito.internal.progress.MockingProgress;
+import org.mockito.internal.progress.MockingProgressImpl;
+import org.mockito.internal.progress.ThreadSafeMockingProgress;
+import org.mockito.internal.util.Primitives;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
@@ -67,6 +77,78 @@ public class VisitAPIServiceTest {
     private static final List<Visit> ZONE_TWO_SORTED_VISITS = List.of(REVISIT_ZONE_TWO_BY_USER_THREE,
             ASSIST_ZONE_TWO_BY_USER_FOUR, TAKE_ZONE_TWO_BY_USER_FOUR_LATER, ASSIST_ZONE_TWO_BY_USER_ONE_LATER,
             ASSIST_ZONE_TWO_BY_USER_TWO_LATER);
+/*
+    static class MyLimit extends Limit {
+        MyLimit(Integer firstRow, Integer maxRows) {
+            super(firstRow, maxRows);
+        }
+
+        @Override
+        public boolean equals(Object that) {
+            if (this == that) {
+                return true;
+            }
+            if (that instanceof MyLimit myLimit) {
+                return Objects.equals(getFirstRow(), myLimit.getFirstRow()) && Objects.equals(getMaxRows(), myLimit.getMaxRows());
+            }
+            if (that instanceof Limit limit) {
+                return Objects.equals(getFirstRow(), limit.getFirstRow()) && Objects.equals(getMaxRows(), limit.getMaxRows());
+            }
+            return false;
+        }
+    }
+
+    @Test
+    public void getSortedVisitsTest_overloadLimit() {
+        when(visitRepository.findAllSorted(any(Limit.class), any())).thenReturn(List.of(new Visit(ZONE_ONE, USER_ONE, TIME_LATER.plusSeconds(7200), VisitType.TAKE)));
+        //when(visitRepository.findAllSorted(Visit.class, new MyLimit(1,100))).thenReturn(List.of(new Visit(ZONE_TWO, USER_TWO, TIME_LATER.plusSeconds(3600), VisitType.TAKE)));
+        when(visitRepository.findAllSorted(new MyLimit(1,100), Visit.class)).thenReturn(SORTED_VISITS_LIST);
+
+        assertEquals(SORTED_VISITS_LIST, visitAPIService.getSortedVisitsBetween(1, 100, Visit.class));
+        verify(visitRepository).findAllSorted(new MyLimit(1, 100), Visit.class);
+
+//        ArgumentCaptor<Class> classCaptor = ArgumentCaptor.forClass(Class.class);
+//        ArgumentCaptor<Limit> limitCaptor = ArgumentCaptor.forClass(Limit.class);
+//        List<Visit> sortedVisitsBetween = visitAPIService.getSortedVisitsBetween(1, 100, Visit.class);
+//        verify(visitRepository).findAllSorted(classCaptor.capture(), limitCaptor.capture());
+//        Class actualClass = classCaptor.getValue();
+//        Limit actualLimit = limitCaptor.getValue();
+//        assertEquals(actualClass, actualLimit);
+    }
+
+    private static void reportMatcher(ArgumentMatcher<?> matcher) {
+        ThreadSafeMockingProgress.mockingProgress().getArgumentMatcherStorage().reportMatcher(matcher);
+    }
+
+    private static Limit eqLimit(Limit value) {
+        reportMatcher(new ArgumentMatcher<Limit>() {
+            @Override
+            public boolean matches(Limit limit) {
+                return Objects.equals(value.getFirstRow(), limit.getFirstRow())
+                        && Objects.equals(value.getMaxRows(), limit.getMaxRows());
+            }
+        });
+        return value == null ? null : value;
+    }
+
+    @Test
+    public void getSortedVisitsTest_ownMatcher() {
+        when(visitRepository.findAllSorted(any(Limit.class), any())).thenReturn(List.of(new Visit(ZONE_ONE, USER_ONE, TIME_LATER.plusSeconds(7200), VisitType.TAKE)));
+        //when(visitRepository.findAllSorted(Visit.class, new MyLimit(1,100))).thenReturn(List.of(new Visit(ZONE_TWO, USER_TWO, TIME_LATER.plusSeconds(3600), VisitType.TAKE)));
+        when(visitRepository.findAllSorted(eqLimit(new Limit(1,100)), eq(Visit.class))).thenReturn(SORTED_VISITS_LIST);
+
+        assertEquals(SORTED_VISITS_LIST, visitAPIService.getSortedVisitsBetween(1, 100, Visit.class));
+        verify(visitRepository).findAllSorted(eqLimit(new Limit(1,100)), eq(Visit.class));
+
+//        ArgumentCaptor<Class> classCaptor = ArgumentCaptor.forClass(Class.class);
+//        ArgumentCaptor<Limit> limitCaptor = ArgumentCaptor.forClass(Limit.class);
+//        List<Visit> sortedVisitsBetween = visitAPIService.getSortedVisitsBetween(1, 100, Visit.class);
+//        verify(visitRepository).findAllSorted(classCaptor.capture(), limitCaptor.capture());
+//        Class actualClass = classCaptor.getValue();
+//        Limit actualLimit = limitCaptor.getValue();
+//        assertEquals(actualClass, actualLimit);
+    }
+*/
 
     @Test
     public void getSortedVisitsTest() {
