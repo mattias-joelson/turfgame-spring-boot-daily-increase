87c63ce4f37932a3cd3a3552dffdabc33c54577d
Mattias Joëlson <mattias@joelson.org>
2024-07-09 17:34:27 +0200

    New user progress model, one entity per date.

---
+++ src/main/java/org/joelson/turf/dailyinc/model/UserProgressTypeProgress.java
@@ -0,0 +1,73 @@
+package org.joelson.turf.dailyinc.model;
+
+import jakarta.persistence.Embeddable;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Embeddable
+public class UserProgressTypeProgress {
+
+    private Integer previous;
+    private Integer completed;
+    private Instant time;
+
+    protected UserProgressTypeProgress() {
+    }
+
+    public UserProgressTypeProgress(
+            Integer previous, Integer completed, Instant time) {
+        this.previous = ModelConstraintsUtil.isEqualOrAboveZero(previous);
+        setCompleted(completed);
+        setTime(time);
+    }
+
+    public Integer getPrevious() {
+        return previous;
+    }
+
+    public Integer getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Integer completed) {
+        this.completed = ModelConstraintsUtil.isEqualOrBelow(
+                ModelConstraintsUtil.isEqualOrAbove(ModelConstraintsUtil.isAboveZero(completed), this.completed),
+                this.previous + 1);
+    }
+
+    public Instant getTime() {
+        return time;
+    }
+
+    public void setTime(Instant time) {
+        this.time = ModelConstraintsUtil.isEqualOrAbove(ModelConstraintsUtil.isTruncatedToSeconds(time), this.time);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o instanceof UserProgressTypeProgress that) {
+            return Objects.equals(previous, that.previous) && Objects.equals(completed, that.completed)
+                    && Objects.equals(time, that.time);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(previous, completed, time);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("UserProgressTypeProgress[previous=%d, completed=%d, time=%s]", previous, completed, time);
+    }
+
+    public String toInnerString(String type) {
+        return String.format("%s.previous=%d, %s.completed=%d, %s.time=%s", type, previous, type, completed, type,
+                time);
+    }
+}
--- src/main/java/org/joelson/turf/dailyinc/service/UserProgressService.java
+++ src/main/java/org/joelson/turf/dailyinc/service/UserProgressService.java
@@ -5,11 +5,13 @@ import org.joelson.turf.dailyinc.model.UserProgress;
 import org.joelson.turf.dailyinc.model.UserProgressId;
 import org.joelson.turf.dailyinc.model.UserProgressRepository;
 import org.joelson.turf.dailyinc.model.UserProgressType;
+import org.joelson.turf.dailyinc.model.UserProgressTypeProgress;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.function.Function;
 
 @Service
 public class UserProgressService {
@@ -17,49 +19,87 @@ public class UserProgressService {
     @Autowired
     UserProgressRepository userProgressRepository;
 
-    private UserProgress getUserProgress(User user, UserProgressType type, Instant date) {
-        return userProgressRepository.findById(new UserProgressId(user.getId(), type, date)).orElse(null);
+    private UserProgress getUserProgress(User user, Instant date) {
+        return userProgressRepository.findById(new UserProgressId(user.getId(), date)).orElse(null);
     }
 
     public int increaseUserProgress(User user, Instant date, int visits, Instant time) {
-        int maxDayCompleted = -1;
-        for (UserProgressType type : UserProgressType.values()) {
-            int dayCompleted = increaseUserProgress(user, type, date, visits, time);
-            if (dayCompleted > maxDayCompleted) {
-                maxDayCompleted = dayCompleted;
-            }
-        }
-        return maxDayCompleted;
-    }
-
-    private int increaseUserProgress(User user, UserProgressType type, Instant date, int visits, Instant time) {
-        UserProgress userProgress = getUserProgress(user, type, date);
+        UserProgress userProgress = getUserProgress(user, date);
         if (userProgress == null) {
             Instant previousDate = date.minus(1, ChronoUnit.DAYS);
-            UserProgress previousUserProgress = getUserProgress(user, type, previousDate);
+            UserProgress previousUserProgress = getUserProgress(user, previousDate);
             if (previousUserProgress == null) {
-                userProgressRepository.save(new UserProgress(user, type, date, 0, 1, time));
+                userProgressRepository.save(new UserProgress(user, date, new UserProgressTypeProgress(0, 1, time),
+                        new UserProgressTypeProgress(0, 1, time), new UserProgressTypeProgress(0, 1, time),
+                        new UserProgressTypeProgress(0, 1, time)));
                 return 1;
-            } else if (type == UserProgressType.DAILY_FIBONACCI) {
-                userProgressRepository.save(
-                        new UserProgress(user, type, date, previousUserProgress.getDayCompleted(), 2, time));
-                return 2;
             } else {
-                userProgressRepository.save(
-                        new UserProgress(user, type, date, previousUserProgress.getDayCompleted(), 1, time));
-                return 1;
+                userProgressRepository.save(new UserProgress(user, date,
+                        new UserProgressTypeProgress(previousUserProgress.getIncrease().getCompleted(), 1, time),
+                        new UserProgressTypeProgress(previousUserProgress.getAdd().getCompleted(), 1, time),
+                        new UserProgressTypeProgress(previousUserProgress.getFibonnaci().getCompleted(), 2, time),
+                        new UserProgressTypeProgress(previousUserProgress.getPowerOfTwo().getCompleted(), 1, time)));
+                return 2;
             }
         } else {
-            Integer dayCompleted = userProgress.getDayCompleted();
-            if (dayCompleted <= userProgress.getPreviousDayCompleted()) {
-                int visitsNeeded = type.getNeededVisits(dayCompleted + 1);
-                if (visits >= visitsNeeded) {
-                    userProgress.setDayCompleted(dayCompleted + 1);
-                    userProgress.setTimeCompleted(time);
-                    userProgressRepository.save(userProgress);
-                }
+            int maxDayCompleted = -1;
+            boolean updated = false;
+
+            int increaseCompleted = increaseUserProgress(userProgress.getIncrease(), visits, time,
+                    UserProgressType.DAILY_INCREASE::getNeededVisits);
+            if (increaseCompleted > 0) {
+                updated = true;
+                maxDayCompleted = Math.max(maxDayCompleted, increaseCompleted);
+            } else {
+                maxDayCompleted = Math.max(maxDayCompleted, -increaseCompleted);
             }
-            return userProgress.getDayCompleted();
+
+            int addCompleted = increaseUserProgress(userProgress.getAdd(), visits, time,
+                    UserProgressType.DAILY_ADD::getNeededVisits);
+            if (increaseCompleted > 0) {
+                updated = true;
+                maxDayCompleted = Math.max(maxDayCompleted, addCompleted);
+            } else {
+                maxDayCompleted = Math.max(maxDayCompleted, -addCompleted);
+            }
+
+            int fiboniacciCompleted = increaseUserProgress(userProgress.getFibonnaci(), visits, time,
+                    UserProgressType.DAILY_FIBONACCI::getNeededVisits);
+            if (increaseCompleted > 0) {
+                updated = true;
+                maxDayCompleted = Math.max(maxDayCompleted, fiboniacciCompleted);
+            } else {
+                maxDayCompleted = Math.max(maxDayCompleted, -fiboniacciCompleted);
+            }
+
+            int powerCompleted = increaseUserProgress(userProgress.getPowerOfTwo(), visits, time,
+                    UserProgressType.DAILY_POWER_OF_TWO::getNeededVisits);
+            if (increaseCompleted > 0) {
+                updated = true;
+                maxDayCompleted = Math.max(maxDayCompleted, powerCompleted);
+            } else {
+                maxDayCompleted = Math.max(maxDayCompleted, -powerCompleted);
+            }
+
+            if (updated) {
+                userProgressRepository.save(userProgress);
+            }
+            return maxDayCompleted;
         }
+    }
+
+    private int increaseUserProgress(
+            UserProgressTypeProgress userProgressTypeProgress, int visits, Instant time,
+            Function<Integer, Integer> neededVisits) {
+        Integer completed = userProgressTypeProgress.getCompleted();
+        if (completed <= userProgressTypeProgress.getPrevious()) {
+            int visitsNeeded = neededVisits.apply(completed + 1);
+            if (visits >= visitsNeeded) {
+                userProgressTypeProgress.setCompleted(completed + 1);
+                userProgressTypeProgress.setTime(time);
+                return completed + 1;
+            }
+        }
+        return -userProgressTypeProgress.getCompleted();
     }
 }
