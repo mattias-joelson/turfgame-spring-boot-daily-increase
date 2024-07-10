f95bbd59e44914dadb743bc77859f882cbc76610
Mattias Joëlson <mattias@joelson.org>
2024-07-10 14:21:29 +0200

    User progress for user can omit the actual user.

--- src/main/java/org/joelson/turf/dailyinc/projection/UserIdAndNameProgress.java
+++ src/main/java/org/joelson/turf/dailyinc/projection/UserIdAndNameProgress.java
@@ -1,13 +1,10 @@
 package org.joelson.turf.dailyinc.projection;
 
-import org.joelson.turf.dailyinc.model.UserProgressType;
 import org.joelson.turf.dailyinc.model.UserProgressTypeProgress;
 
 import java.time.Instant;
 
 public interface UserIdAndNameProgress {
-
-    UserIdAndName getUser();
 
     Instant getDate();
 
