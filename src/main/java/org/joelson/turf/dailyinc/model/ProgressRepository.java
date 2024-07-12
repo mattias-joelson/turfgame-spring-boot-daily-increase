23c9b149b82bec8843a6d01adfe79f85962e803c
Mattias Joëlson <mattias@joelson.org>
2024-07-12 12:59:54 +0200

    Renaming of Progress up to p.

--- src/main/java/org/joelson/turf/dailyinc/model/ProgressRepository.java
+++ src/main/java/org/joelson/turf/dailyinc/model/ProgressRepository.java
@@ -9,19 +9,19 @@ import java.util.List;
 
 public interface ProgressRepository extends JpaRepository<Progress, ProgressId> {
 
-    @Query("select up from Progress up order by up.user.id, up.date limit :limit offset :offset")
+    @Query("select p from Progress p order by p.user.id, p.date limit :limit offset :offset")
     @QueryHints(@QueryHint(name = "org.hibernate.fetchSize", value = "100"))
     <T> List<T> findSortedBetween(int offset, int limit, Class<T> type);
 
-    @Query("select up from Progress up order by up.user.id desc, up.date desc limit :limit")
+    @Query("select p from Progress p order by p.user.id desc, p.date desc limit :limit")
     @QueryHints(@QueryHint(name = "org.hibernate.fetchSize", value = "100"))
     <T> List<T> findLastSortedReversed(int limit, Class<T> type);
 
-    @Query("select up from Progress up where up.user.id = :userId order by up.date limit :limit offset :offset")
+    @Query("select p from Progress p where p.user.id = :userId order by p.date limit :limit offset :offset")
     @QueryHints(@QueryHint(name = "org.hibernate.fetchSize", value = "100"))
     <T> List<T> findSortedBetweenByUser(Long userId, int offset, int limit, Class<T> type);
 
-    @Query("select up from Progress up where up.user.id = :userId order by up.date desc limit :limit")
+    @Query("select p from Progress p where p.user.id = :userId order by p.date desc limit :limit")
     @QueryHints(@QueryHint(name = "org.hibernate.fetchSize", value = "100"))
     <T> List<T> findLastSortedReversedByUser(Long userId, int limit, Class<T> type);
 }
